8832d64394d61bb9a2265d7edaa83c838c2f9d91
rojarbel <rojarbel@gmail.com>
2025-07-10 08:55:40 +0300

    rev

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -21,7 +21,7 @@ export default {
     associatedDomains: ["applinks:rota.app"],
     supportsTablet: true,
     config: {
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY_IOS
     }
   },
   android: {
@@ -36,7 +36,7 @@ export default {
     ],
     config: {
       googleMaps: {
-        apiKey: process.env.GOOGLE_MAPS_API_KEY
+        apiKey: process.env.GOOGLE_MAPS_API_KEY_ANDROID
       }
     }
   },
