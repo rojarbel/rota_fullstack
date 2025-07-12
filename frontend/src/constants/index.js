f5d43ace8f60e0383be187a96e094b4f2c44013a
rojarbel <rojarbel@gmail.com>
2025-07-12 09:48:35 +0300

    rev

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -2,5 +2,7 @@ import Constants from 'expo-constants';
 
 export const IMAGE_BASE_URL =
   Constants.expoConfig?.extra?.imageCdnUrl ||
+  Constants.manifest?.extra?.imageCdnUrl ||
   Constants.expoConfig?.extra?.apiUrl ||
+  Constants.manifest?.extra?.apiUrl ||
   '';
