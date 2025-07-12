fe26e3ff3a227aa26942901006b88d33f1b52550
rojarbel <rojarbel@gmail.com>
2025-07-12 08:48:31 +0300

    rev

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,64 +1,66 @@
 import 'dotenv/config';
 
 export default {
-  name: "UrbanRota",
-  slug: "urbanrota",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/images/icon.png",
-  userInterfaceStyle: "automatic",
+  name: 'Rota',
+  slug: 'urbanrota',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  userInterfaceStyle: 'automatic',
   splash: {
-    image: "./assets/images/splash-icon.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff"
+    image: './assets/images/splash-icon.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
   },
   updates: {
-    fallbackToCacheTimeout: 0
+    fallbackToCacheTimeout: 0,
   },
-  assetBundlePatterns: ["**/*"],
+  assetBundlePatterns: ['**/*'],
   ios: {
-    bundleIdentifier: "com.rojar.urbanrota",
-    associatedDomains: ["applinks:rota.app"],
+    bundleIdentifier: 'com.rojar.urbanrota',
+    associatedDomains: ['applinks:rota.app'],
     supportsTablet: true,
     config: {
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY_IOS
-    }
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY_IOS,
+    },
   },
   android: {
-    package: "com.rojar.urbanrota",
-    permissions: [],
+    package: 'com.rojar.urbanrota',
+    permissions: ['ACCESS_FINE_LOCATION', 'ACCESS_COARSE_LOCATION'],
     newArchEnabled: false,
     intentFilters: [
       {
-        action: "VIEW",
-        data: { scheme: "https", host: "rota.app", pathPrefix: "/etkinlik" },
-        category: ["BROWSABLE", "DEFAULT"],
+        action: 'VIEW',
+        data: {
+          scheme: 'https',
+          host: 'rota.app',
+          pathPrefix: '/etkinlik',
+        },
+        category: ['BROWSABLE', 'DEFAULT'],
       },
     ],
-      config: {
-        googleMaps: {
-          apiKey: process.env.GOOGLE_MAPS_API_KEY_ANDROID
-        }
-      }
-        },
+    config: {
+      googleMaps: {
+        apiKey: process.env.GOOGLE_MAPS_API_KEY_ANDROID,
+      },
+    },
+  },
   extra: {
     apiUrl: process.env.API_URL,
     imageCdnUrl: process.env.IMAGE_CDN_URL,
-    googleMapsApiKey:
-      process.env.GOOGLE_MAPS_API_KEY_ANDROID ||
-      process.env.GOOGLE_MAPS_API_KEY_IOS,
+    googleMapsApiKeyAndroid: process.env.GOOGLE_MAPS_API_KEY_ANDROID,
+    googleMapsApiKeyIos: process.env.GOOGLE_MAPS_API_KEY_IOS,
     eas: {
-      projectId: "92308120-fad1-43c5-875a-ca78967dbf13"
-    }
+      projectId: '92308120-fad1-43c5-875a-ca78967dbf13',
+    },
   },
-
   plugins: [
-    "expo-router",
-    "expo-splash-screen",
-    "./plugins/withGoogleMapsString"
+    'expo-router',
+    'expo-splash-screen',
+    './plugins/withGoogleMapsString',
   ],
   experiments: {
-    typedRoutes: true
+    typedRoutes: true,
   },
-    scheme: "urbanrota",
+  scheme: 'urbanrota',
 };
