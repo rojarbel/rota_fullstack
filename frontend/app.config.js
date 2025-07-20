afd5e0798fbba0ebf7244b3fc58673619b27db12
rojarbel <rojarbel@gmail.com>
2025-07-20 15:37:54 +0300

    rev

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -42,50 +42,47 @@ export default {
       NSLocationAlwaysAndWhenInUseUsageDescription: "This app does not track location in the background. Location access is used only while the app is open to show nearby events. The data is not stored or shared.",
     },
   },
-  android: {
-    package: 'com.rojar.rota',
-    permissions: ['ACCESS_FINE_LOCATION', "READ_MEDIA_IMAGES", 'ACCESS_COARSE_LOCATION'],
-    adaptiveIcon: {
-      foregroundImage: "./assets/images/adaptive-icon.png",
-      backgroundColor: "#ffffff"
+android: {
+  package: 'com.rojar.rota',
+  permissions: ['ACCESS_FINE_LOCATION', 'READ_MEDIA_IMAGES', 'ACCESS_COARSE_LOCATION'],
+  adaptiveIcon: {
+    foregroundImage: './assets/images/adaptive-icon.png',
+    backgroundColor: '#ffffff',
+  },
+  edgeToEdgeEnabled: true,
+  compileSdkVersion: 35,
+  targetSdkVersion: 35,
+  intentFilters: [
+    {
+      action: 'VIEW',
+      data: {
+        scheme: 'https',
+        host: 'rota.app',
+        pathPrefix: '/etkinlik',
+      },
+      category: ['BROWSABLE', 'DEFAULT'],
     },
-    edgeToEdgeEnabled: true,
-    compileSdkVersion: 35,
-    targetSdkVersion: 35,
-    intentFilters: [
-      {
-        action: 'VIEW',
-        data: {
-          scheme: 'https',
-          host: 'rota.app',
-          pathPrefix: '/etkinlik',
-        },
-        category: ['BROWSABLE', 'DEFAULT'],
-      },
-    ],
-    config: {
-      googleMaps: {
-        apiKey: process.env.GOOGLE_MAPS_API_KEY_ANDROID || 'YOUR_ANDROID_API_KEY',
-      },
+  ],
+  config: {
+    googleMaps: {
+      apiKey: process.env.GOOGLE_MAPS_API_KEY_ANDROID || 'YOUR_ANDROID_API_KEY',
     },
   },
+},
+
   web: {
     bundler: "metro",
     output: "static",
     favicon: "./assets/images/favicon.png"
   },
   extra: {
-    apiUrl: process.env.API_URL || 'https://your-api-url.com',
-    imageCdnUrl: process.env.IMAGE_CDN_URL || 'https://your-cdn-url.com',
-    googleMapsApiKey:
-      process.env.GOOGLE_MAPS_API_KEY_ANDROID ||
-      process.env.GOOGLE_MAPS_API_KEY_IOS ||
-      'YOUR_API_KEY',
-          googleMapsApiKeyAndroid: process.env.GOOGLE_MAPS_API_KEY_ANDROID || 'YOUR_ANDROID_API_KEY',
-    googleMapsApiKeyIos: process.env.GOOGLE_MAPS_API_KEY_IOS || 'YOUR_IOS_API_KEY',
+    googleMapsApiKeyAndroid: process.env.GOOGLE_MAPS_API_KEY_ANDROID,
+    googleMapsApiKeyIos: process.env.GOOGLE_MAPS_API_KEY_IOS,
+    apiUrl: process.env.API_URL,
+    imageCdnUrl: process.env.IMAGE_CDN_URL,
     eas: {
-      projectId: "81fe7a04-a148-44d3-9753-bda2ea2752e6"
-    }
+      projectId: '81fe7a04-a148-44d3-9753-bda2ea2752e6',
+    },
   },
   plugins: [
     'expo-router',
