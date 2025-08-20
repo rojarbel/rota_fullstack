ab10d17670c83b5a3a33e87ad20932b35a3f2ee0
rojarbel <rojarbel@gmail.com>
2025-08-20 08:55:55 +0300

    rev

--- frontend/plugins/withGoogleMapsString.js
+++ frontend/plugins/withGoogleMapsString.js
@@ -1,4 +1,5 @@
-const { withDangerousMod, withAndroidManifest, AndroidConfig } = require('@expo/config-plugins');
+const { withDangerousMod, withAndroidManifest, AndroidConfig } = require('expo/config-plugins');
+
 const fs = require('fs');
 const path = require('path');
 
